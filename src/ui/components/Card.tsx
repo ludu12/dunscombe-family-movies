a2d02e1e2f789500ab12ec0e2ed67126eb823821
Luke Dunscombe <dunscombe1@gmail.com>
2024-03-24 09:28:51 -0500

    Fixing overflow

--- src/ui/components/Card.tsx
+++ src/ui/components/Card.tsx
@@ -10,9 +10,9 @@ export function Card({
   shortDescription: string;
 }) {
   return (
-    <div className="card flex-grow bg-base-100 shadow-xl">
+    <div className="card flex-grow bg-base-100 shadow-xl overflow-hidden">
       <div className="card-body">
-        <h3 className="text-sm italic">{name}</h3>
+        <h3 className="text-sm italic overflow-hidden overflow-ellipsis whitespace-nowrap">{name}</h3>
         <h2 className="card-title">{shortDescription}</h2>
         <p
           className={
